4cb0e8adff6becb924d734463480e0f19cf90561
SebastianSoto17 <sebastian.soto@cetys.edu.mx>
2025-05-21 16:40:46 -0700

    refactor alv

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
         self.pos = 0
+        self.indent_stack = [0]
 
     def current(self):
         if self.pos < len(self.tokens):
@@ -23,7 +24,7 @@ class Parser:
             stmt = self.parse_statement()
             if stmt:
                 statements.append(stmt)
-        return statements
+        return NodeFactory.create('block', statements)
 
     def parse_statement(self):
         token_type, value, line = self.current()
@@ -45,6 +46,20 @@ class Parser:
 
         elif token_type == 'IF':
             return self.parse_if()
+            
+        elif token_type == 'WHILE':
+            return self.parse_while()
+            
+        elif token_type == 'INDENT':
+            self.match('INDENT')
+            return None
+            
+        elif token_type == 'DEDENT':
+            self.match('DEDENT')
+            return None
+
+        elif token_type == 'EOF':
+            return None
 
         else:
             raise SyntaxError(f"Unexpected token {token_type} at line {line}")
@@ -56,12 +71,47 @@ class Parser:
         self.match('INDENT')
 
         body = []
-        while self.current()[0] != 'DEDENT':
+        while self.current()[0] != 'DEDENT' and self.current()[0] != 'EOF':
             stmt = self.parse_statement()
-            body.append(stmt)
+            if stmt:
+                body.append(stmt)
 
-        self.match('DEDENT')
-        return NodeFactory.create('if', condition, body)
+        if self.current()[0] == 'DEDENT':
+            self.match('DEDENT')
+        
+        # Check for else clause
+        else_body = None
+        if self.current()[0] == 'ELSE':
+            self.match('ELSE')
+            self.match('COLON')
+            self.match('INDENT')
+            
+            else_body = []
+            while self.current()[0] != 'DEDENT' and self.current()[0] != 'EOF':
+                stmt = self.parse_statement()
+                if stmt:
+                    else_body.append(stmt)
+                
+            if self.current()[0] == 'DEDENT':
+                self.match('DEDENT')
+            
+        return NodeFactory.create('if', condition, body, else_body)
+
+    def parse_while(self):
+        self.match('WHILE')
+        condition = self.parse_expression()
+        self.match('COLON')
+        self.match('INDENT')
+
+        body = []
+        while self.current()[0] != 'DEDENT' and self.current()[0] != 'EOF':
+            stmt = self.parse_statement()
+            if stmt:
+                body.append(stmt)
+
+        if self.current()[0] == 'DEDENT':
+            self.match('DEDENT')
+        return NodeFactory.create('while', condition, body)
 
     def parse_expression(self):
         # Comparaciones como x > 3 o a == b
@@ -101,6 +151,9 @@ class Parser:
         elif token_type == 'STRING':
             self.match('STRING')
             return NodeFactory.create('string', value)
+        elif token_type in ('TRUE', 'FALSE'):
+            self.match(token_type)
+            return NodeFactory.create('boolean', value)
         elif token_type == 'ID':
             self.match('ID')
             return NodeFactory.create('identifier', value)
