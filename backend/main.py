8d58727fcad04beb51596f185c2edbfcef6c9230
Kicho115 <oscar.leonardo.ang@gmail.com>
2025-05-26 11:29:12 -0700

    Update frontend url in cors

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["https://minipython-lexer-and-parser-flx1.vercel.app/"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
