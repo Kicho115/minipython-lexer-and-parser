d0acdcfad8c5f353b59078e1d2c2867ec5b6bddd
Kicho115 <oscar.leonardo.ang@gmail.com>
2025-05-26 11:17:18 -0700

    Add .vercelignore to exclude backend and tester.py; update API URL in Home component to use environment variable

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
   const handleCompile = async () => {
     try {
-      const response = await fetch("http://localhost:8000/compile", {
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/compile", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
